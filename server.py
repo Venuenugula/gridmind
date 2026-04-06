57510a3293a1b76ebaeb1ef529b98c0131ad28d6
venuenugula <venuenugula9@gmail.com>
2026-04-06 16:02:25 +0530

    fix: proper entrypoint for openenv

--- server.py
+++ server.py
@@ -21,3 +21,10 @@ async def step(action: dict):
 @app.get("/state")
 def state():
     return env.state().dict()
+def main():
+    import uvicorn
+    uvicorn.run("server:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
